1d87295913da7e860560f7c000bb9cd99206524a
Piyush Sharma <Piyush.sharma@blu-smart.com>
2025-05-12 01:28:53 +0530

    Initial Commit

---
+++ PhotoTranslatorNew/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PhotoTranslatorNew
+//
+//  Created by Piyush on 12/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PhotoTranslatorNew/PhotoTranslatorNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoTranslatorNewApp.swift
+//  PhotoTranslatorNew
+//
+//  Created by Piyush on 12/05/25.
+//
+
+import SwiftUI
+
+@main
+struct PhotoTranslatorNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
